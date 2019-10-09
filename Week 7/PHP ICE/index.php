f659cde2bfd398b815debf9e2b43448ee00fc0a4
Adit-COCO-Garg <adit.garg21k@gmail.com>
2019-10-09 18:20:45 -0400

    GIT ALL

---
+++ Week 7/PHP ICE/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>php info</title>
+</head>
+<body>
+<?php
+    echo"<h1>hello World</h1>";
+    echo"<p>It's" . Date('r') . "</p>";
+    phpinfo();
+    echo "<h1 title = \"Hello Hello\">Hello!</h1>";
+    echo "<p><a href = \"http://www.amazon.com\">Link to Amazon</a></p>"
+?>
+    
+</body>
+</html>
