a27ea4a17fcc2e87071821740ad3d278a826f687
Adit-COCO-Garg <adit.garg21k@gmail.com>
2019-12-02 18:26:18 -0500

    darkmode-custom

--- PROJECT 2/js/script.js
+++ PROJECT 2/js/script.js
@@ -1,3 +1,4 @@
+const THEME = ["#f0f0f0","#090029", true, true,];
 const API_PARAMS = {
     query: "&q=",
     query_limit: "&limit=10",
@@ -10,9 +11,28 @@ const API_PARAMS = {
     colc:""
 };
 window.onload = (e) => {
-    document.querySelector("#search").addEventListener("click", searchButtonClicked)
-    // document.querySelector("#search").addEventListener("click", searchButtonClicked)
+    document.querySelector("#searchterm").addEventListener("keydown", searchButtonClicked);
+    document.querySelector("#darkmode-toggle").addEventListener("mouseover", toggleDarkMouseOver);
+    document.querySelector("#darkmode-toggle").addEventListener("mouseleave", toggleDarkMouseOver);
+    document.querySelector("#darkmode-toggle").addEventListener("click", toggleDarkclick);
 };
+
+
+
+function toggleDarkMouseOver(){
+    this.style.backgroundColor = THEME[3] ? THEME[0] : THEME[1];
+    THEME[3] = !THEME[3];
+}
+
+
+function toggleDarkclick(){
+    this.style.backgroundColor = THEME[2] ? THEME[0] : THEME[1];
+    document.querySelector("body").style.backgroundColor =  THEME[2] ? THEME[0] : THEME[1];
+    THEME[2] = !THEME[2];
+    THEME[3] = !THEME[3];
+}
+
+
 // /* The function checks if the prototype method trim exists or not
 //  * https://medium.com/@ugwuraphael/javascript-properties-and-methods-string-prototype-trim-7806d765188
 //  * Raphael Ugwu
@@ -24,62 +44,66 @@ window.onload = (e) => {
 //     };
 //     }
 
-async function searchButtonClicked(){
-    if (API_PARAMS.q_time!=0){
-        API_PARAMS.colc.destroy();
-        document.querySelectorAll(".grid-col").forEach(e=>{e.innerHTML=""});
-        await temp(); 
-        API_PARAMS.q_time=0;
-        API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
-            columns: '.grid-col',
-            items: '.grid-item'
-        });
-    } else {
-        await temp(); 
-        API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
-            columns: '.grid-col',
-            items: '.grid-item'
-        });
-        API_PARAMS.q_time=0;
-    }
-    // if (!(API_PARAMS.q_time==0)) {
-    //     // variable is undefined or null
-    //     API_PARAMS.q_time=0;
-    // }
-    API_PARAMS.q_time=1;
-}
-
-async function temp(){
-    temp_term = (encodeURIComponent(document.querySelector("#searchterm").value).trim());
-    if (temp_term.length<1) {
-        console.log("No search provided");
-    }
-    else {
-        API_PARAMS.query += temp_term
-        API_PARAMS.search_url += API_PARAMS.query + API_PARAMS.query_limit;
-        let response = await fetch(API_PARAMS.search_url);
-        let json = await response.json();
-        let grid = document.querySelector(".grid");
-        grid.innerHTML=`<section class="grid-col grid-col--1"></section>
-        <section class="grid-col grid-col--2"></section>
-        <section class="grid-col grid-col--3"></section>
-        <section class="grid-col grid-col--4"></section>`;
-        if (!json.data || json.data.length ==0){
-            console.log("not found!");
-        }
-        else{
-            const gifResults = json.data;
-            await gifResults.forEach(obj=>{
-                let d = document.createElement("div")
-                d.className ="grid-item";
-                d.innerHTML=`<img src="${obj.images.fixed_height.url}" alt="${obj.source}" class="giphy">`;
-                grid.appendChild(d);
+async function searchButtonClicked(e){
+    if (e.keyCode ==13){
+        if (API_PARAMS.q_time!=0){
+            API_PARAMS.colc.destroy();
+            document.querySelectorAll(".grid-col").forEach(e=>{e.innerHTML=""});
+            await temp(); 
+            API_PARAMS.q_time=0;
+            API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
+                columns: '.grid-col',
+                items: '.grid-item'
             });
-            API_PARAMS.query = "&q=",
-            API_PARAMS.search_url = "https://api.giphy.com/v1/gifs/search?"+"api_key=md8TKXmklb61Iwgx5ARcazARdFESrjUy"
-            
-            
-            // selector string as first argument
+        } else {
+            await temp(); 
+            API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
+                columns: '.grid-col',
+                items: '.grid-item'
+            });
+            API_PARAMS.q_time=0;
         }
+        // if (!(API_PARAMS.q_time==0)) {
+        //     // variable is undefined or null
+        //     API_PARAMS.q_time=0;
+        // }
+        API_PARAMS.q_time=1;
     }
+    
+    async function temp(){
+        temp_term = (encodeURIComponent(document.querySelector("#searchterm").value).trim());
+        if (temp_term.length<1) {
+            console.log("No search provided");
+        }
+        else {
+            API_PARAMS.query += temp_term
+            API_PARAMS.search_url += API_PARAMS.query + API_PARAMS.query_limit;
+            let response = await fetch(API_PARAMS.search_url);
+            let json = await response.json();
+            let grid = document.querySelector(".grid");
+            grid.innerHTML=`<section class="grid-col grid-col--1"></section>
+            <section class="grid-col grid-col--2"></section>
+            <section class="grid-col grid-col--3"></section>
+            <section class="grid-col grid-col--4"></section>`;
+            if (!json.data || json.data.length ==0){
+                console.log("not found!");
+            }
+            else{
+                const gifResults = json.data;
+                await gifResults.forEach(obj=>{
+                    let d = document.createElement("div")
+                    d.className ="grid-item";
+                    d.innerHTML=`<img src="${obj.images.fixed_height.url}" alt="${obj.source}" class="giphy">`;
+                    grid.appendChild(d);
+                });
+                API_PARAMS.query = "&q=",
+                API_PARAMS.search_url = "https://api.giphy.com/v1/gifs/search?"+"api_key=md8TKXmklb61Iwgx5ARcazARdFESrjUy"
+                
+                
+                // selector string as first argument
+            }
+        }
+
+    }
+    
 }
