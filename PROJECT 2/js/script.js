442ddda53ea17c3619466c0550c9e365fa40720e
Adit-COCO-Garg <adit.garg21k@gmail.com>
2019-12-04 13:34:04 -0500

    Added Pure CSS darkmode

--- PROJECT 2/js/script.js
+++ PROJECT 2/js/script.js
@@ -5,13 +5,19 @@ const API_PARAMS = {
     rating: "g",
     lang: "en",
     random_id: "oof",
-    search_url: "https://api.giphy.com/v1/gifs/search?"+"api_key=md8TKXmklb61Iwgx5ARcazARdFESrjUy",
-    q_time:0,
-    colc:""
+    search_url: "https://api.giphy.com/v1/gifs/search?" + "api_key=md8TKXmklb61Iwgx5ARcazARdFESrjUy",
+    q_time: 0,
+    colc: ""
 };
+
+
 window.onload = (e) => {
-    document.querySelector("#search").addEventListener("click", searchButtonClicked)
-    // document.querySelector("#search").addEventListener("click", searchButtonClicked)
+    document.querySelector("#searchterm").addEventListener("keyup", event=>{
+        if (event.key == "Enter"){
+            searchButtonClicked();
+        }
+    });
+
 };
 // /* The function checks if the prototype method trim exists or not
 //  * https://medium.com/@ugwuraphael/javascript-properties-and-methods-string-prototype-trim-7806d765188
@@ -24,61 +30,62 @@ window.onload = (e) => {
 //     };
 //     }
 
-async function searchButtonClicked(){
-    if (API_PARAMS.q_time!=0){
-        API_PARAMS.colc.destroy();
-        document.querySelectorAll(".grid-col").forEach(e=>{e.innerHTML=""});
-        await temp(); 
-        API_PARAMS.q_time=0;
-        API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
-            columns: '.grid-col',
-            items: '.grid-item'
-        });
-    } else {
-        await temp(); 
-        API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
-            columns: '.grid-col',
-            items: '.grid-item'
-        });
-        API_PARAMS.q_time=0;
-    }
-    // if (!(API_PARAMS.q_time==0)) {
-    //     // variable is undefined or null
-    //     API_PARAMS.q_time=0;
-    // }
-    API_PARAMS.q_time=1;
+async function searchButtonClicked(e) {
+        if (API_PARAMS.q_time != 0) {
+            API_PARAMS.colc.destroy();
+            document.querySelectorAll(".grid-col").forEach(e => {
+                e.innerHTML = ""
+            });
+            await temp();
+            API_PARAMS.q_time = 0;
+            API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
+                columns: '.grid-col',
+                items: '.grid-item'
+            });
+        } else {
+            await temp();
+            API_PARAMS.colc = new Colcade(document.querySelector(".grid"), {
+                columns: '.grid-col',
+                items: '.grid-item'
+            });
+            API_PARAMS.q_time = 0;
+        }
+        // if (!(API_PARAMS.q_time==0)) {
+        //     // variable is undefined or null
+        //     API_PARAMS.q_time=0;
+        // }
+        API_PARAMS.q_time = 1;
+    
 }
 
-async function temp(){
+async function temp() {
     temp_term = (encodeURIComponent(document.querySelector("#searchterm").value).trim());
-    if (temp_term.length<1) {
+    if (temp_term.length < 1) {
         console.log("No search provided");
-    }
-    else {
+    } else {
         API_PARAMS.query += temp_term
         API_PARAMS.search_url += API_PARAMS.query + API_PARAMS.query_limit;
         let response = await fetch(API_PARAMS.search_url);
         let json = await response.json();
         let grid = document.querySelector(".grid");
-        grid.innerHTML=`<section class="grid-col grid-col--1"></section>
+        grid.innerHTML = `<section class="grid-col grid-col--1"></section>
         <section class="grid-col grid-col--2"></section>
         <section class="grid-col grid-col--3"></section>
         <section class="grid-col grid-col--4"></section>`;
-        if (!json.data || json.data.length ==0){
+        if (!json.data || json.data.length == 0) {
             console.log("not found!");
-        }
-        else{
+        } else {
             const gifResults = json.data;
-            await gifResults.forEach(obj=>{
+            await gifResults.forEach(obj => {
                 let d = document.createElement("div")
-                d.className ="grid-item";
-                d.innerHTML=`<img src="${obj.images.fixed_height.url}" alt="${obj.source}" class="giphy">`;
+                d.className = "grid-item";
+                d.innerHTML = `<img src="${obj.images.fixed_height.url}" alt="${obj.source}" class="giphy">`;
                 grid.appendChild(d);
             });
             API_PARAMS.query = "&q=",
-            API_PARAMS.search_url = "https://api.giphy.com/v1/gifs/search?"+"api_key=md8TKXmklb61Iwgx5ARcazARdFESrjUy"
-            
-            
+                API_PARAMS.search_url = "https://api.giphy.com/v1/gifs/search?" + "api_key=md8TKXmklb61Iwgx5ARcazARdFESrjUy"
+
+
             // selector string as first argument
         }
     }
